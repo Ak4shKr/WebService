bf2b5207d727491f97ce3fb0dce0241a8ce707cc
Ak4shKr <akashkmr7667@gmail.com>
2024-02-06 08:34:35 +0530

    final commit

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { menu, close } from "../assets/icons";
 
 const Header = () => {
@@ -13,6 +13,14 @@ const Header = () => {
   const toggleIcon = () => {
     setIconToggle(!iconToggle);
   };
+  useEffect(() => {
+    setMenuClass(
+      toggle
+        ? "transition-opacity ease-in-out duration-300 transform translate-y-0 opacity-100 "
+        : "transition-opacity ease-in-out duration-300 transform transalate-y-full opacity-0"
+    );
+  }, [toggle]);
+
   return (
     <section className="w-full bg-black text-white flex justify-between items-center px-8 py-3 lg:px-16 sticky top-0 z-40 ">
       <h1 className="text-green-600 text-3xl font-bold ">DEBUG ENTITY</h1>
@@ -83,6 +91,30 @@ const Header = () => {
               toggleIcon();
             }}
           />
+        )}
+        {toggle && (
+          <div
+            id="mob-menu"
+            className={`bg-green-500 text-white py-4 absolute top-16 right-0 w-full ${menuClass}`}
+          >
+            <ul className="flex flex-col justify-center items-center gap-2">
+              <li className="text-xl text-white font-semibold hover:bg-green-600 hover:text-white w-full py-3 text-center">
+                <a href="/">Home</a>
+              </li>
+              <li className="text-xl text-white font-semibold hover:bg-green-600 hover:text-white w-full py-3 text-center">
+                <a href="#services">Services</a>
+              </li>
+              <li className="text-xl text-white font-semibold hover:bg-green-600 hover:text-white w-full py-3 text-center">
+                <a href="#pricing">Pricing</a>
+              </li>
+              <li className="text-xl text-white font-semibold hover:bg-green-600 hover:text-white w-full py-3 text-center">
+                <a href="#testimonials">Testimonials</a>
+              </li>
+              <li className="text-xl text-white font-semibold hover:bg-green-600 hover:text-white w-full py-3 text-center">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
         )}
       </div>
     </section>
